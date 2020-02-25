ba949a1a9fa71e80802bc0b398730111827d9729
Tanveesh Singh Chaudhery <Tanveeshs@gmail.com>
2020-02-25 19:47:13 +0530

    cloudDB

--- routes/userModel.js
+++ routes/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/indent2");
+// mongoose.connect("mongodb://localhost:27017/indent2");
+mongoose.connect("mongodb+srv://newUser:pass1234@indent-pavbm.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true});
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
